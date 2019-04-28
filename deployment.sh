8480be7a2d5da4d0a87c484ffb5e5b6e846247ef
Simona Cotin <simona.cotin@gmail.com>
2019-04-28 13:18:02 +0300

    oups 🔑

--- deployment.sh
+++ deployment.sh
@@ -4,3 +4,11 @@ az group deployment create \
     --resource-group graphless11 \
     --template-file azuredeploy.json \
     --parameters azuredeploy.parameters.json
+
+az webapp cors add -g graphless11 -n cspace11 --allowed-origins https://cspaceclient11.z6.web.core.windows.net
+
+az extension add --name storage-preview
+
+az storage blob service-properties update --account-name cspaceclient11 --static-website --404-document index.html --index-document index.html
+
+az storage blob upload-batch -s graphiql/index.html -d \$web --account-name cspaceclient11
